6fbc5127f9b5d1b90e01b18b84f3bfdba557cf84
Clifton Palmer <cliftonpalmer@pop-os>
2019-06-18 21:00:18 -0500

    public.sh now makes sure container is running

--- public.sh
+++ public.sh
@@ -3,6 +3,8 @@ service=public_web
 
 for f in $(docker service ps -q $service)
 do
+    [[ running == $(docker inspect --format '{{.Status.State}}' $f) ]] || continue
     container_id=$(docker inspect --format '{{.Status.ContainerStatus.ContainerID}}' $f)
+    echo docker exec $container_id /root/public "$@"
     docker exec $container_id /root/public "$@"
 done
