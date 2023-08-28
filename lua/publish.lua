78944fffed51306762c335cbd7c6e16170dad9e3
Clifton Palmer <clifton.james.palmer@protonmail.com>
2023-08-28 04:49:53 -0500

    Updated publish with FQDN override

--- lua/publish.lua
+++ lua/publish.lua
@@ -25,9 +25,13 @@ function handle(r)
 
     if pattern then
         local nonce = publish(r.context_document_root, pattern)
-        local uri = ("%s://%s:%s/%s"):format(
-            r.is_https and "https" or "http", 
-            r.server_name, r.port, nonce)
+
+        -- if FQDN override is set,
+        -- use override instead of my own server name and port number
+        local fqdn = os.getenv('PUBLIC_FQDN_OVERRIDE')
+        fqdn = fqdn or (r.is_https and "https" or "http") .. "://" .. r.server_name .. ":" ..r.port
+
+        local uri = ("%s/%s"):format(fqdn, nonce)
         r:puts( ([[<a href="%s">%s</a>]]):format(uri, uri) )
     else
         local template = get_form(r.context_document_root)
